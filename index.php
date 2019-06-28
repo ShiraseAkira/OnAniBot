609ba72a6dc6bbbdd65f8ce6e6ed7cfaf471a0ad
Maxim <justsaynya@gmail.com>
2019-06-28 16:03:09 +0300

    hello world bot

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
-    echo 'Hello World!';
+include('vendor/autoload.php'); //Подключаем библиотеку
+use Telegram\Bot\Api;
+
+$telegram = new Api('639677299:AAEIo8bfRnC5axKEUuJG1l_LuBSHLmSD3ao'); //Устанавливаем токен, полученный у BotFather
+$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+
+$text = $result["message"]["text"]; //Текст сообщения
+$chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
+$name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+$keyboard = [["Hello World!"], ["Hello, username"]]; //Клавиатура
+
+if($text){
+    if ($text == "/start") {
+        $reply = "Добро пожаловать в бота!";
+        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+    }elseif ($text == "Hello World!") {
+        $reply = "Hello World!";
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+    }elseif ($text == "Hello, username") {
+        if($name){
+            $reply = "Hello, " . $name;
+        }else{
+            $reply = "Hello, anon";
+        }
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+    }
+}else{
+    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+}
 ?>
