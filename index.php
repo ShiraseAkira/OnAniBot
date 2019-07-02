af1232486b8ce8e52d0f98ab32b40c30067550d1
Maxim <justsaynya@gmail.com>
2019-07-03 00:24:49 +0300

    Added update.php

--- index.php
+++ index.php
@@ -3,14 +3,15 @@ include('vendor/autoload.php'); //Подключаем библиотеку
 use Telegram\Bot\Api;
 
 $telegram = new Api('639677299:AAEIo8bfRnC5axKEUuJG1l_LuBSHLmSD3ao'); //Устанавливаем токен, полученный у BotFather
-$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+$result = $telegram->getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
 
 $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
 $keyboard = [["Hello World!"], ["Hello, username"], ["Next anime"]]; //Клавиатура
 
-function getNextAnime(){
+function getNextAnime(): string
+{
     // create curl resource
     $ch = curl_init();
     // set url
@@ -26,26 +27,25 @@ function getNextAnime(){
     return $data[0]->anime->name;
 }
 
-if($text){
+if ($text) {
     if ($text == "/start") {
         $reply = "Добро пожаловать в бота!";
-        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-    }elseif ($text == "Hello World!") {
+        $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+    } elseif ($text == "Hello World!") {
         $reply = "Hello World!";
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-    }elseif ($text == "Hello, username") {
-        if($name){
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
+    } elseif ($text == "Hello, username") {
+        if ($name) {
             $reply = "Hello, " . $name;
-        }else{
+        } else {
             $reply = "Hello, anon";
         }
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-    }elseif ($text == "Next anime") {
-        $reply = "Ближайшее выхожящее в эфир аниме: \"".getNextAnime()."\"";
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
+    } elseif ($text == "Next anime") {
+        $reply = "Ближайшее выхожящее в эфир аниме: \"" . getNextAnime() . "\"";
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
     }
-}else{
-    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+} else {
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение."]);
 }
-?>
