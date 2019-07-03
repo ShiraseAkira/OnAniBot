328c2daad8167ef2a46876a258d49579ec36153d
Maxim <justsaynya@gmail.com>
2019-07-03 15:54:48 +0300

    refactoring

--- database.php
+++ database.php
@@ -3,8 +3,8 @@ const dbHost = "eu-cdbr-west-02.cleardb.net";
 const dbUsername = "b2b48db1e8befd";
 const dbPasswd = "8113a8b7";
 const dbName = "heroku_717c9367403bbb5";
-function getDatabaseConnection() {
 
+function getDatabaseConnection() {
     $mysqlli = new mysqli(dbHost, dbUsername, dbPasswd, dbName);
     if($mysqlli->connect_errno) {
         error_log("Ошибка: " . $mysqlli->connect_errno);
@@ -12,4 +12,36 @@ function getDatabaseConnection() {
     } else {
         return $mysqlli;
     }
+}
+
+function getDataFromDatabase($database, $sql) {
+    if(!$result = $database->query($sql)) {
+        error_log("Error: ".$sql.PHP_EOL.$database->error);
+        return NULL;
+    } else {
+        return $result;
+    }
+}
+
+function updateDataInDatabase($database, $sql) {
+    if(!$database->query($sql)) {
+        error_log("Error: ".$sql.PHP_EOL.$database->error);
+        return NULL;
+    } else {
+        return TRUE;
+    }
+}
+
+function getMailingList($database) {
+    $sql = "SELECT users.chatid, anime.name, anime.episodesAired
+        FROM `users`, `watchlist`, `notifications`, `anime`
+        WHERE users.chatid = watchlist.chatid
+        AND watchlist.watchlistid = notifications.notificationid
+        AND anime.shikiid = watchlist.shikiid";
+    return getDataFromDatabase($database, $sql);
+}
+
+function truncateMailingList($database) {
+    $sql = "TRUNCATE `notifications`";
+    return updateDataInDatabase($database, $sql);
 }
--- mailing.php
+++ mailing.php
@@ -1,27 +1,14 @@
 <?php
 require_once("database.php");
-$databse = getDatabaseConnection();
-
-$sql = "SELECT users.chatid, anime.name, anime.episodesAired
-        FROM `users`, `watchlist`, `notifications`, `anime`
-        WHERE users.chatid = watchlist.chatid
-        AND watchlist.watchlistid = notifications.notificationid
-        AND anime.shikiid = watchlist.shikiid";
-
-if(!$result = $databse->query($sql)) {
-    error_log("Error: ".$sql.PHP_EOL.$databse->error);
-}
-
-$sql = "TRUNCATE `notifications`";
-if(! $databse->query($sql)) {
-    error_log("Error: ".$sql.PHP_EOL.$databse->error);
-}
+$database = getDatabaseConnection();
+$mailingList = getMailingList($database);
+truncateMailingList($database);
 
 include('vendor/autoload.php'); //Подключаем библиотеку
 use Telegram\Bot\Api;
 $telegram = new Api('639677299:AAEIo8bfRnC5axKEUuJG1l_LuBSHLmSD3ao'); //Устанавливаем токен, полученный у BotFather
 
-while($message = $result->fetch_object()){
+while($message = $mailingList->fetch_object()){
     $reply = "Вышла ".$message->episodesAired." серия ".$message->name.PHP_EOL;
     $telegram->sendMessage(['chat_id' => $message->chatid, 'text' => $reply]);
 }
